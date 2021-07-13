6abe65703276b9344e348d4862fd1bae1807bfec
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-13 15:02:00 +0100

    Add pseudo-code.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello");
+// Main task:
+// - ask the user what is their age;
+// - ask the user how far they are traveling (in km);
+// - the price per 1km is £0.21;
+// - if the user is under 18, they get 20% discount;
+// - if the user is over 65, they get 40% discount;
+// - the user between 18 and 65 pay full price.
